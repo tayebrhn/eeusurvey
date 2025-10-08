ba83c9e7b692135afe977c37d5ab88567e055be0
tayebrhn <tayyebirhan@gmail.com>
2025-10-08 09:39:20 +0300

    add and configure chart.js

--- eeusurvey/settings.py
+++ eeusurvey/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'eeusurvey.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Make sure this line exists
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,6 +127,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Collects all app-level and global static files
+STATICFILES_DIRS = [
+    BASE_DIR / "static",   # global static
+]
+
+# For production (collected output)
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
